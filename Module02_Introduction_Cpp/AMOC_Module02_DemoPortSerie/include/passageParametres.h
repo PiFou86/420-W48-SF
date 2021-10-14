e4a487b617dae6b51fdfedb952fcdcf7fc5c9816
Pierre-François Léon <pierrefrancois.leon@gmail.com>
2021-10-14 10:11:40 -0400

    Correction liens

--- Module02_Introduction_Cpp/AMOC_Module02_DemoPortSerie/include/passageParametres.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "Flasher.h"
-
-void passageDeParametres1(int p_v1, int &p_r1, int *p_p1);
-void passageDeParametresCopie(Flasher p_flasher);
-void passageDeParametresReference(Flasher& p_flasher);
-void passageDeParametresPointeur(Flasher* p_flasher);
